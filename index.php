f464004e09c4fa9c96153bbe12a27515ec233ade
efgoetsch <egoetsch@mail.greenriver.edu>
2019-02-12 01:39:08 -0500

    second commit

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-/*
+<!--
     Ellen Goetsch
     February 11, 2019
     index.php
-*/
+-->
 
 <?php
 
     //connect to database
+require '/home/egoetsch/config.php';
     try {
         //instantiate a database object
-        $dbh = new PDO("mysql:dbname=egoetsch_grc",
-            "egoetsch", "DeeJay07912");
+        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
         echo 'Connected to database!';
     } catch(PDOException $e) {
         echo $e->getMessage();

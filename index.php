f9513b23b4e2685df4ac16a231ccf8540a748c8a
efgoetsch <egoetsch@mail.greenriver.edu>
2019-02-12 02:23:30 -0500

    seventh commit

--- index.php
+++ index.php
@@ -73,3 +73,18 @@ echo "<p>Pet $id inserted successfully.</p>";
     $statement->execute();
     $id = $dbh->lastInsertId();
     echo "<p>Pet $id inserted successfully.</p>";
+
+    //define the query
+    $sql = "UPDATE pets SET name = :new WHERE name = :old";
+
+    //prepare the statement
+    $statement = $dbh->prepare($sql);
+
+    //bind the parameters
+    $old = 'Joey';
+    $new = 'Troy';
+    $statement->bindParam(':old', $old, PDO::PARAM_STR);
+    $statement->bindParam(':new', $new, PDO::PARAM_STR);
+
+    //execute
+    $statement->execute();

0e7806e153c8925eaf3952a46453914dd81b80d4
efgoetsch <egoetsch@mail.greenriver.edu>
2019-02-12 01:58:16 -0500

    third commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php
 
     //connect to database
-require '/home/egoetsch/config.php';
+    require '/home/egoetsch/config.php';
     try {
         //instantiate a database object
         $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
@@ -15,3 +15,20 @@ require '/home/egoetsch/config.php';
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
+
+    //define the query
+    $sql = "INSERT INTO pets(type, name, color) VALUES (:type, :name, :color)";
+
+    //prepare the statement
+    $statement = $dbh->prepare($sql);
+
+    //bind the parameters
+    $type = 'kangaroo';
+    $name = 'Joey';
+    $color = 'purple';
+    $statement->bindParam(':type', $type, PDO::PARAM_STR);
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $statement->bindParam(':color', $color, PDO::PARAM_STR);
+
+    //execute
+$statement->execute();
